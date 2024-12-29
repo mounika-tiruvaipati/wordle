4b05360ed4b4c6e514062f5428eadee5bb96dc56
mounika-tiruvaipati <mounikatiruvaipati25@gmail.com>
2024-12-28 23:42:52 -0600

    Updated logic

--- wordle_logic.py
+++ wordle_logic.py
@@ -1,1 +1,18 @@
-"This is a test."
+def matching_words(word, guess):
+    example = []
+    for g, w in zip(guess, word):
+        if g == w:
+            example.append(g)
+
+    return example
+
+word = "Happy"
+guess = input("Please enter your guess: ")
+
+while guess != word:
+    result = matching_words(word, guess)
+    print("Matching letters:", result)
+    
+    guess = input("Please enter your next guess: ")
+
+print("Congratulations! You've guessed the word.")
